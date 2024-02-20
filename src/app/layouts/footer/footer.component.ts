d3287a13236fd8ed9265a58ea909c26ea2abdb09
Nouné BILBOULIAN <nouneblb@gmail.com>
2024-02-20 19:10:34 +0100

    footer bootstrap + schedule ts

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  schedules = [
+    { day: 'Lun - Ven', timeRanges: ['08:00 - 13:00', '14:00 - 20:00'] },
+    { day: 'Sam', timeRanges: ['9:00 - 18:00'] },
+    { day: 'Dim', closed: true },
+  ];
 }
